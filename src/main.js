209bbef9ed6120b4fda3ed6c01373627fd32f21c
Murilo Augusto <mu10-2015@hotmail.com>
2022-10-20 22:12:24 -0300

    functions update informations

--- src/main.js
+++ src/main.js
@@ -66,8 +66,39 @@ const cardNumberPattern = {
     const foundMask = dynamicMasked.compiledMasks.find(function (item) {
       return number.match(item.regex)
     })
-    console.log(foundMask)
+
     return foundMask
   },
 }
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+const addButton = document.querySelector('#add-card')
+addButton.addEventListener('click', () => {
+  alert('Cartão adicionado!=')
+})
+
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault()
+})
+
+const cardHolder = document.querySelector('#card-holder')
+cardHolder.addEventListener('input', () => {
+  const ccHolder = document.querySelector('.cc-holder .value')
+  ccHolder.innerText = cardHolder.value.length === 0 || cardHolder.value.match(/^\s/) ? 'Fulano da Silva' : cardHolder.value
+})
+
+securityCodeMasked.on('accept', () => {
+  updateSecurityCode(securityCodeMasked.value)
+})
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector('.cc-security .value')
+  ccSecurity.innerText = code.length === 0 ? '123' : code
+}
+
+cardNumberMasked.on('accept', () => {
+  updateCardNuber(cardNumberMasked.value)
+})
+function updateCardNuber(number) {
+  const ccNumber = document.querySelector('.cc-info .cc-number')
+  ccNumber.innerText = number.length === 0 ? '1234 5678 9012 3456' : number
+}
